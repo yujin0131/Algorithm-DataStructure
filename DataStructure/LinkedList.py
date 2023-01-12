a29a97ac7f8f7a6c8d1d66a801bda948ff475213
yujin0131 <lyujin0131@naver.com>
2023-01-12 14:40:11 +0900

    Add LinkedList.py

--- DataStructure/LinkedList.py
+++ DataStructure/LinkedList.py
@@ -5,8 +5,7 @@ class Node:                   # Node 클래스 정의
 
 # Create LinkedList
 class LinkedList:
-    def __init__(self, value):
-        newNode = Node(value)
+    def __init__(self):
         self.head = None
         self.tail = None
         self.length = 1
@@ -29,16 +28,86 @@ class LinkedList:
         self.length += 1
         return True
 
-    def pop(self, value):
-        newNode = Node(value)
+    def pop(self):
         if self.length == 0:
             return None
-        temp = newNode
+        temp = self.head
         pre = self.head
-        self.head.next = pre
+        while temp.next:
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp
+
+    def prepend(self,value):
+        newNode = Node(value)
+        if self.head is None:
+            self.head = newNode
+            self.tail = newNode
+        else:
+            newNode.next = self.head
+            self.head = newNode
+        self.length += 1
+        return True
+
+    def popFirst(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        self.head = self.head.next
+        temp.next = None
+        self.length -= 1
+        return temp
+
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head
+        for _ in range(index): # _ : 값이 딱히 상관없을 경우 언더스코어(_)로 표시 
+            temp = temp.next
+        return temp
+
+    def set_value(self, index, value):
+        temp = self.get(index)
+        temp.value = value
+        return True
+
+    def insert(self, index, value):
+        # if index < 0 or index >= self.length:
+        #     return None
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length - 1:
+            return self.append(value)
+
+        newNode = Node(value) # 받아온 노드
+        temp = self.get(index - 1) # 원래 해당 index 값 temp 노드에 옮김
+        newNode.next = temp.next # 원래 index.next 를 newNode.next 에 옮김
+        temp.next = newNode # temp.next 에 새로운 newNode
+
+        self.length += 1
+        return True
+
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.popFirst
+        if index == self.length - 1 :
+            return self.pop
+        
+        prev = self.get(index - 1)
+        temp = prev.next
+        prev.next = temp.next
+        temp.next = None
+        self.length -= 1
+        return temp
         
 
-# node1 = Node(3)
 
-# print(node1.data)
-# print(node1.next)
